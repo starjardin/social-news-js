1fafca0968f3f897b590ab14a0896a81298d6c72
Tantely Andrianarivola <tantely.and@onja.org>
2020-06-17 11:59:31 +0300

    added new project called social news

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// write your code here
-console.log('it works!');
+//Created an array of an object to store the existing links
 const links = [
   {
     title: "Madagascar news",
@@ -18,18 +17,37 @@ const links = [
   },
 ];
 
+//A function to display the existing links, will need it for later.
 const linkList = () => {
   for (let i = 0; i < links.length; i++) {
-    let myLink = `${links[i].title} ${links[i].url} ${links[i].author}`;
+    let myLink = `Title: ${links[i].title}
+Url: ${links[i].url}
+Author: ${links[i].author}`;
     alert(myLink)
   }
 };
 
+//A function to allow the user to enter their news. We'll call it later.
 const addNweLink = () => {
-  const linTtitle = prompt("Enter here your link title");
+  const linkTitle = prompt("Enter here your link title");
   const linkAuthor = prompt("Enter here your link Author");
   const linkUrl = prompt("Enter here your link url");
-  links.title.push(linkTitle)
+  let news = {
+    title: linkTitle ,
+    author : linkAuthor,
+    url : linkUrl
+  };
+  if (url.startsWith !== '"http' && url.endsWith !== '://') {
+      news.url.push(`"http://" ${linkUrl}`)
+  };
+  links.push(news)
+  return;
+}
+
+const deletedNews = () => {
+  let indexNewsToDelete = Number(prompt("Enter here the rank of the news you want to delete"));
+  let deletedOneNews = links.splice(indexNewsToDelete - 1, 1);
+  alert(`we have deleted the news titled: ${deletedOneNews[0].title}`)
 }
 
 const startMenuString = `Choose an option:
@@ -39,24 +57,22 @@ const startMenuString = `Choose an option:
 0 : Quit`
 
 let menuStart = Number(prompt(startMenuString));
-
+//while loop to allow the user to read or enter new news.
 while(menuStart !== 0) {
-  switch(menuStart) {
-    case 1:
-      linkList();
-      break;
-    case 2:
-      addNweLink();
-      break;
-    case 3:
-      alert("Do you want to delerte a link");
-      break;
-    default:
-      alert("Sorry not found");
-      break;
+  //if the user enter "1" it will display the existing news(we are calling function here)
+  if (menuStart === 1) {
+    linkList();
+  //if the user enter "2" it will allow them to enter new news(we're calling function here)  
+  } else if (menuStart === 2) {
+    addNweLink();
+  //if the user enter "3" it will allow the user to delete news form the existing news.(we're calling function here)  
+  } else if (menuStart === 3) {
+    deletedNews();
+  //if none of the above it will ask them again to enter a new number.  
+  } else {
+    alert("Sorry not found, please try again with a number between 0 to 4");
   }
   menuStart = Number(prompt(startMenuString));
 }
-alert("Good bye")
-console.log(menuStart);
-
+//If the user enter 0, end of the program.
+alert("Good bye");
